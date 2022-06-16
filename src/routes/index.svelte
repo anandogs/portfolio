f9a0a4a9c8ea67cc23abf3aa3586bd32402ce91d
Anando Ghose <anandoghose@gmail.com>
2022-06-16 12:01:21 +0530

    working checkout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,35 @@
-
-
-<script>
-    	let catPC = '/images/landing_cat.png';
+<script lang="ts">
+	let catPC = '/images/landing_cat.png';
+    
+    let education: HTMLElement;
+    
+	import IoIosArrowDown from 'svelte-icons/io/IoIosArrowDown.svelte';
+    const scrollToLocation = (scrollLoc:HTMLElement) => {
+        scrollLoc.scrollIntoView({behavior: "smooth", block: "end"})
+    }
 </script>
 
 <main>
-    <div class="h-screen">
-    <div class="h-[90%] bg-[#030006] grid justify-items-center content-center px-[44px] gap-y-[70px]">
-        <h1 class="text-white">
-            Hello, World.
-        </h1>
-        
-        <img src={catPC} alt='A cat in a PC' class="w-[178px]">
-        <h2 class="text-white text-center max-w-[278px]">I’m Anando - 
-            I’m interested in computers, <span class="italic">mostly</span>.
-        </h2>
-    </div>
-        <div class="h-[10%] bg-gradient-to-b from-[#030006] to-[#ECE7C2]">
-    </div>
-    
-</div>
+	<section class="h-screen">
+		<div
+			class="h-[90%] bg-[#030006] grid justify-items-center content-center px-[44px] gap-y-[60px]"
+		>
+			<h1 class="text-white">Hello, World.</h1>
+
+			<img src={catPC} alt="A cat in a PC" class="w-[178px]" />
+			<h2 class="text-white text-center max-w-[278px]">
+				I’m Anando - I’m interested in computers, <span class="italic">mostly</span>.
+			</h2>
+			<button class="text-white w-[25px] cursor-pointer animate-bounce" on:click={() => scrollToLocation(education)}>
+				<IoIosArrowDown />
+            </button>
+		</div>
+		<div class="h-[10%] bg-gradient-to-b from-[#030006] to-[#ECE7C2]" />
+	</section>
+	<section class="h-screen" bind:this={education}>
+		<div
+			class="h-[90%] bg-[#ECE7C2] grid justify-items-center content-center px-[44px] gap-y-[60px]"
+		/>
+		<div class="h-[10%] bg-gradient-to-b from-[#ECE7C2] to-[#06010B]" />
+	</section>
 </main>
