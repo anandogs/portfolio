06d20536ec215406b0d4fbea74d705f0d351c3ae
Anando Ghose <anandoghose@gmail.com>
2022-06-16 10:49:54 +0530

    not sure, resetting sigh

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,9 @@
+<script>
+	import IoIosMenu from 'svelte-icons/io/IoIosMenu.svelte';
+</script>
+
+<nav class="absolute top-0 w-screen py-8 px-[10%] flex justify-end">
+	<div class="w-[25px] text-white">
+		<IoIosMenu />
+	</div>
+</nav>
