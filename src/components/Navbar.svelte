9757a27d103040bf648333f7978f13384f9b771e
Anando Ghose <anandoghose@gmail.com>
2022-06-16 13:05:31 +0530

    updated with navbar and id card

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,9 +1,10 @@
-<script>
+<script lang="ts">
 	import IoIosMenu from 'svelte-icons/io/IoIosMenu.svelte';
+
 </script>
 
 <nav class="absolute top-0 w-screen py-8 px-[10%] flex justify-end">
-	<div class="w-[25px] text-white">
+	<button class="w-[25px] text-white">
 		<IoIosMenu />
-	</div>
+	</button>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
 	let catPC = '/images/landing_cat.png';
-    
+	let nerd = '/images/nerd.png';
     let education: HTMLElement;
-    
+    import Navbar from '../components/Navbar.svelte';
 	import IoIosArrowDown from 'svelte-icons/io/IoIosArrowDown.svelte';
     const scrollToLocation = (scrollLoc:HTMLElement) => {
         scrollLoc.scrollIntoView({behavior: "smooth", block: "end"})
     }
+
+
 </script>
 
 <main>
+	<Navbar/>
 	<section class="h-screen">
 		<div
-			class="h-[90%] bg-[#030006] grid justify-items-center content-center px-[44px] gap-y-[60px]"
+			class="h-[90%] pt-10 bg-[#030006] grid justify-items-center content-center px-[44px] gap-y-[60px]"
 		>
 			<h1 class="text-white">Hello, World.</h1>
 
-			<img src={catPC} alt="A cat in a PC" class="w-[178px]" />
+			<img src={catPC} alt="A cat in a PC" class='w-[178px]'/>
 			<h2 class="text-white text-center max-w-[278px]">
 				I’m Anando - I’m interested in computers, <span class="italic">mostly</span>.
 			</h2>
@@ -29,7 +32,10 @@
 	<section class="h-screen" bind:this={education}>
 		<div
 			class="h-[90%] bg-[#ECE7C2] grid justify-items-center content-center px-[44px] gap-y-[60px]"
-		/>
+		>
+		<h2 class="text-center">I mean I’m <span class="italic">qualified</span> for other stuff</h2>
+		<img src={nerd} alt="A nerd guy" class='w-[150px]'/>
+		</div>
 		<div class="h-[10%] bg-gradient-to-b from-[#ECE7C2] to-[#06010B]" />
 	</section>
 </main>
