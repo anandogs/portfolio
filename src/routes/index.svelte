39191a0402bacc0615cdc77f995baa8ec5b1c3b0
Anando Ghose <anandoghose@gmail.com>
2022-06-16 08:09:33 +0530

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+
+
+<script>
+    	let catPC = '/images/landing_cat.png';
+</script>
+
+<main>
+    <div class="h-screen">
+    <div class="h-[90%] bg-[#030006] grid justify-items-center content-center px-[44px] gap-y-[70px]">
+        <h1 class="text-white">
+            Hello, World.
+        </h1>
+        
+        <img src={catPC} alt='A cat in a PC' class="w-[178px]">
+        <h2 class="text-white text-center max-w-[278px]">I’m Anando - 
+            I’m interested in computers, <span class="italic">mostly</span>.
+        </h2>
+    </div>
+        <div class="h-[10%] bg-gradient-to-b from-[#030006] to-[#ECE7C2]">
+    </div>
+    
+</div>
+</main>
