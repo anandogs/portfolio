9004aabfd0dcc311bb87d6362e81c37bb2e426aa
Anando Ghose <anandoghose@gmail.com>
2022-08-17 20:18:28 +0530

    changed prerender settings

--- svelte.config.js
+++ svelte.config.js
@@ -12,12 +12,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		}),
+		adapter: adapter(),
 		prerender: {
 			// This can be false if you're using a fallback (i.e. SPA mode)
 			default: true
